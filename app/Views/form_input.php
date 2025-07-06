5cc8d9ef44337df6b20441e4be98a66da6d07829
globog3 <adipriyo45@gmail.com>
2025-07-06 23:30:40 +0700

    Delete app/Views directory

--- app/Views/form_input.php
+++
@@ -1,6 +0,0 @@
-function form_input($data = '', $value = '', $extra = '')
-{
-    $defaults = ['type' => 'text', 'name' => is_array($data) ? '' : $data];
-
-    return "<input "._parse_form_attributes($data, $defaults).$extra." />";
-}
--- app/Views/form_password.php
+++
@@ -1,9 +0,0 @@
-<?php
-function form_password($data = '', $value = '', $extra = '')
-{
-    if (is_array($data)) {
-        $data['type'] = 'password';
-    } else {
-        $data = ['type' => 'password', 'name' => $data];
-    }
-}
